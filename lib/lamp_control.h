7f19d2649581db9b0a73d79de6ebe173d71c4483
Maarten Vandersteegen <maarten.vandersteegen@altran.com>
2017-04-16 11:17:45 +0200

    Fix in travis file + forgot to call curl global init/deinit

--- lib/lamp_control.h
+++ lib/lamp_control.h
@@ -16,31 +16,31 @@ struct LampControl
 };
 
 /*
- *  self:  lamp control instance 
+ *  self:  lamp control instance
  */
 int lamp_control_init(struct LampControl *self);
 
 /*
- *  self:  lamp control instance 
+ *  self:  lamp control instance
  */
 void lamp_control_deinit(struct LampControl *self);
 
 /*
  *  Set the state of the lamp
- *  self:  lamp control instance 
+ *  self:  lamp control instance
  *  state:  aggregate build state to be displayed on the lamp
  */
 void lamp_control_set_state(struct LampControl *self, enum BuildState build_state);
 
 /*
  *  Signal program error on the lamp
- *  self:  lamp control instance 
+ *  self:  lamp control instance
  */
 void lamp_control_signal_error(struct LampControl *self);
 
 /*
  *  Turn of the lamp
- *  self:  lamp control instance 
+ *  self:  lamp control instance
  */
 void lamp_control_off(struct LampControl *self);
 
